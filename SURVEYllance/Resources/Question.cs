cfbea41a255a20fc310b2efaa3565fbbecf08553
Benedikt <benedikt.geiger00@gmail.com>
2021-11-08 15:29:39 +0100

    [Backend]  - Forgot to add Question.cs to d52358e203f5472419f04596c3eb3ce9cbcb9329

--- SURVEYllance/Resources/Question.cs
+++ SURVEYllance/Resources/Question.cs
@@ -1,11 +1,24 @@
-﻿namespace SURVEYllance.Resources
+﻿using System;
+
+namespace SURVEYllance.Resources
 {
     public class Question
     {
         /// <summary>
+        /// Id to identify the question from frontend calls
+        /// </summary>
+        private readonly string _id;
+        
+        /// <summary>
         /// Title of this question
         /// </summary>
         private readonly string _title;
+        
+        /// <summary>
+        /// Getter for <see cref="_id"/>
+        /// Id is generated on construction
+        /// </summary>
+        public string Id => _id;
         
         /// <summary>
         /// Getter for <see cref="_title"/>
@@ -19,6 +32,7 @@
         /// <param name="title">Title of this question</param>
         public Question(string title)
         {
+            _id = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
             _title = title;
         }
         
